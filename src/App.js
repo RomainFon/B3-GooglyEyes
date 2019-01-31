048311231beb48e66401e94b6cf005ffebd2a6a6
RomainFon <romain.fon974@gmail.com>
2019-01-31 16:42:43 +0100

    Add menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import GooglyEyes from './components/googlyEyes'
+import Menu from './components/menu'
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            menu: false
+        }
+    }
+    toggleMenu(){
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
 
     takePicture(){
         this.refs.googlyEye.picture()
@@ -10,8 +22,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <Menu display={ this.state.menu }/>
                 <GooglyEyes ref={"googlyEye"}></GooglyEyes>
-                <button onClick={() => this.takePicture()}>PICTURE</button>
+                <div className={this.state.menu ? "button_menu active" : "button_menu"} onClick={() => this.toggleMenu()}><div className="hamburger"></div></div>
+                <button className={"button_picture"} onClick={() => this.takePicture()}>PICTURE</button>
             </div>
         );
     }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
-import GooglyEyes from "./googlyEyes";
+import mainLogo from'./../logoMDS.jpg';
+
 
 class Menu extends Component {
-
+    render(){
+        return(
+            <div className={this.props.display ? "menu open" : "menu"}>
+                <img className={"logoMDS"} src={mainLogo} />
+            </div>
+            )
+    }
 }
 
 export default Menu;
